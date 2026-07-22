ff6fe24ea77f49f9ff6205365fec30a63709cd02
GalaxyS24 FE Hype <144151505+s24fethebest@users.noreply.github.com>
2026-07-22 16:08:35 +0300

    Update build_flashable_zip.sh

--- scripts/internal/build_flashable_zip.sh
+++ scripts/internal/build_flashable_zip.sh
@@ -32,10 +32,10 @@ $ROM_IS_OFFICIAL && ROM_STATUS="OFFICIAL"
 ZIP_FILE_SUFFIX="-sign.zip"
 $DEBUG && ! $ROM_IS_OFFICIAL && ZIP_FILE_SUFFIX=".zip"
 
-FILE_NAME="ExtremeROM_${ROM_STATUS}_${ROM_VERSION}_$(date +%Y%m%d)_${TARGET_CODENAME}${ZIP_FILE_SUFFIX}"
+FILE_NAME="RubyROM_${ROM_STATUS}_${ROM_VERSION}_$(date +%Y%m%d)_${TARGET_CODENAME}${ZIP_FILE_SUFFIX}"
 while [ -f "$OUT_DIR/$FILE_NAME" ]; do
     INCREMENTAL=$((INCREMENTAL + 1))
-    FILE_NAME="ExtremeROM_${ROM_VERSION}_$(date +%Y%m%d)-${INCREMENTAL}_${TARGET_CODENAME}${ZIP_FILE_SUFFIX}"
+    FILE_NAME="RubyROM_${ROM_VERSION}_$(date +%Y%m%d)-${INCREMENTAL}_${TARGET_CODENAME}${ZIP_FILE_SUFFIX}"
 done
 
 PRIVATE_KEY_PATH="$SRC_DIR/security/"
@@ -582,8 +582,9 @@ PRINT_HEADER()
     echo    'ui_print(" ");'
     echo    'ui_print("****************************************************");'
     echo -n 'ui_print("'
-    echo -n "Welcome to ExtremeROM $ROM_CODENAME $ROM_VERSION for $TARGET_NAME!"
+    echo -n "Welcome to RubyROM $ROM_CODENAME $ROM_VERSION for $TARGET_NAME!"
     echo    '");'
+    echo    'ui_print("RubyROM developed by AhmetTheDev0725(s24fethebest@GitHub)");'
     echo    'ui_print("ExtremeROM developed by ExtremeXT @XDAforums");'
     echo    'ui_print("Initial UN1CA build system coded by salvo_giangri @XDAforums");'
     echo    'ui_print("Special thanks to all ExtremeROM Maintainers, Contribuitors and Testers");'
@@ -598,6 +599,11 @@ PRINT_HEADER()
     echo -n "Target: $TARGET_FINGERPRINT"
     echo    '");'
     echo    'ui_print("****************************************************");'
+    echo    'ui_print("Thank you for installing and using RubyROM! <3");'
+    echo    '");'
+    echo    'ui_print("We wish that you have the best experience while");'
+    echo    'ui_print("using RubyROM");'
+    echo    'ui_print("****************************************************");'
     echo    'ui_print("After installation, it is highly recommended to FORMAT DATA as follows:");'
     echo    'ui_print("     Wipe -> Format Data");'
     echo    'ui_print("Hint: FORMAT, not WIPE or FACTORY RESET!");'
@@ -609,13 +615,6 @@ PRINT_HEADER()
     echo    'assert(run_program("/sbin/sh", "-c", "while true; do getevent -lc 1 | grep -q -m1 '\''KEY_VOLUMEUP'\'' && exit 0; sleep 1; done"));'
     echo    'ui_print("Volume UP detected. Proceeding!");'
     echo    'ui_print("****************************************************");'
-    echo    'ui_print("   ____     __                    ___  ____  __  ___");'
-    echo    'ui_print("  / __/_ __/ /________ __ _  ___ / _ \/ __ \/  |/  /");'
-    echo    "ui_print(\" / _/ \ \ / __/ __/ -_)  ' \/ -_) , _/ /_/ / /|_/ / \");"
-    echo    'ui_print("/___//_\_\\\__/_/  \__/_/_/_/\__/_/|_|\____/_/  /_/  ");'
-    echo    'ui_print("                                                    ");'
-    echo    'ui_print("****************************************************");'
-}
 # ]
 
 [ -d "$TMP_DIR" ] && rm -rf "$TMP_DIR"
